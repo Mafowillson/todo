9fb8c624aab429253e90f2e561863661db09e464
unknown <fouotsawilson@gmail.com>
2025-03-22 05:09:51 +0100

    fixes

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'todo'
+
 urlpatterns = [
     path('', views.list_task, name='todo_list'),
     path('todo/create/', views.create_todo, name = 'create_todo'),
